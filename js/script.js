ab917cfd4a5df0fb8eac2ee09317050241145b4a
Denis <d.y.vladimirov@gmail.com>
2020-12-18 21:02:56 +0300

    финалочка

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
-let position = 0;
-const slidesToShow = 1;
-const slidesToScroll  = 1;
-const slider = document.querySelector('.slider');
-const sliderList = slider.querySelector('.slider__list');
-const sliderItem = sliderList.querySelectorAll('.slider__item');
-const prevButton = slider.querySelector('.slider__prev-btn');
-const nextButton = slider.querySelector('.slider__next-btn');
-const dotItems = slider.querySelectorAll('.slider__dots-item');
-const itemWidth= slider.offsetWidth / slidesToShow;
-const movePosition = slidesToScroll * itemWidth;
+const sliderList = document.querySelector('.slider__list');
+const sliderItems = sliderList.querySelectorAll('.slider__item');
+const activeSlide = sliderList.querySelector('.slider__item--active');
+const dotItems = sliderList.querySelectorAll('.slider__dots-item');
+const activeDot = sliderList.querySelector('.slider__dots-item--active');
+const prevButton = sliderList.querySelector('.slider__prev-btn');
+const nextButton = sliderList.querySelector('.slider__next-btn');
+let index = 1;
 
-sliderItem.forEach((item) => item.style.minWidth = `${itemWidth}px`);
-
-const setPrevPhoto = () => {
-  position += movePosition;
-  setPosition();
-  checkButton();
-}
-
-const setNextPhoto = () => {
-  position -= movePosition;
-  setPosition();
-  checkButton();
-}
-
-prevButton.addEventListener('click', setPrevPhoto);
-nextButton.addEventListener('click', setNextPhoto);
-
-const setPosition = () => {
-  sliderList.style.transform = `translateX(${position}px)`;
+const setActive = (index, element, className) => {
+  element.forEach((item) => {
+    item.classList.remove(className);
+  })
+  element[index].classList.add(className);
 };
 
-const checkButton = () => {
-  console.log(position)
-  prevButton.disabled = position === 0;
-  nextButton.disabled = position <= -(sliderItem.length - slidesToShow) * itemWidth;
+const setCurrentSlide = (n) => {
+  setActive(n, sliderItems, 'slider__item--active')
+  setActive(n, dotItems, 'slider__dots-item--active');
 };
-checkButton()
+
+const setNextSlide = () => {
+  if (index === sliderItems.length - 1) {
+    index = 0;
+    setCurrentSlide(index);
+  } else {
+    index++;
+    setCurrentSlide(index);
+  } 
+};
+
+const setPrevSlide = () => {
+  if (index === 0) {
+    index = sliderItems.length - 1;
+    setCurrentSlide(index);
+  } else {
+    index--;
+    setCurrentSlide(index);
+  } 
+};
+
+dotItems.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    index = i;
+    setCurrentSlide(i);
+  })
+})
+
+nextButton.addEventListener('click', setNextSlide);
+prevButton.addEventListener('click', setPrevSlide);
+
+const interval = setInterval(setNextSlide, 4000);
+
+sliderList.addEventListener('click', () => {
+  clearInterval(interval);
+})
